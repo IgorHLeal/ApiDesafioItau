a81d475d2758853e4c614d6daa0452c1e0caa156
Igor Leal <igorhenriqueleal3@gmail.com>
2025-05-06 17:26:22 -0300

    Criados os métodos para Adicionar, Deletar e Obter Transações

---
+++ Services/TransacaoService.cs
@@ -0,0 +1,31 @@
+﻿using ApiDesafioItau.Models;
+
+namespace ApiDesafioItau.Services
+{
+    public class TransacaoService
+    {
+        private readonly List<Transacao> _transacoes = new();
+
+        public void AdicionarTransacao (Transacao transacao)
+        {
+            //lock(_transacoes)
+            //{
+                _transacoes.Add(transacao);
+            //}
+        }
+
+        public void ApagarTransacoes()
+        {
+            _transacoes.Clear();
+        }
+
+        public List<Transacao> OterTransacoesUltimoMinuto()
+        {
+            var date = DateTime.UtcNow;
+
+            return _transacoes
+                .Where(x => x.dataHora >= date.AddSeconds(-60) && x.dataHora <= date)
+                .ToList();
+        }
+    }
+}
